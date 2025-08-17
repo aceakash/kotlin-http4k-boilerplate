ad6c69ce178b3d9224bb26b2670e19ca7962329e
Akash Kurdekar <akash.kurdekar@sedex.com>
2025-08-17 12:04:59 +0100

    can run web server

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.gradle.api.JavaVersion.VERSION_21
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm") version "2.2.0"
+    application
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+application {
+    mainClass = "com.example.HelloWorldKt"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            allWarningsAsErrors = false
+            jvmTarget.set(JVM_21)
+            freeCompilerArgs.add("-Xjvm-default=all")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+
+    java {
+        sourceCompatibility = VERSION_21
+        targetCompatibility = VERSION_21
+    }
+}
+
+dependencies {
+    implementation(platform("org.http4k:http4k-bom:6.15.1.0"))
+    implementation("org.http4k:http4k-core")
+    testImplementation("org.http4k:http4k-testing-approval")
+    testImplementation("org.http4k:http4k-testing-hamkrest")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.13.3")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.13.3")
+}
+
